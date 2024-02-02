daefc820b50e1fb358058e1c58574538b73e47d8
developer <developer@offices-MacBook-Pro.local>
2024-02-02 14:49:36 +0200

    finish: 11 topic

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,41 @@
 import { Server } from "http";
 import express, { Express } from "express";
 
-import { userRouter } from "./users/users";
 import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
+import { ExeptionFilter } from "./errors/exeption.filter";
 
 export class App {
   app: Express;
   port: number;
   server: Server;
   logger: LoggerService;
+  userController: UserController;
+  exeptionFilter: ExeptionFilter;
 
-  constructor(logger: LoggerService) {
+  constructor(
+    logger: LoggerService,
+    userController: UserController,
+    exeptionFilter: ExeptionFilter
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
+    this.userController = userController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   useRoutes() {
-    this.app.use("/users", userRouter);
+    this.app.use("/users", this.userController.router);
+  }
+
+  useExeptionFilters() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
   }
 
   public async init() {
     this.useRoutes();
+    this.useExeptionFilters();
     this.server = this.app.listen(this.port);
 
     this.logger.log(`SERVER ON http://localhost:${this.port}`);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { App } from "./app";
 import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
+import { ExeptionFilter } from "./errors/exeption.filter";
 
 async function bootstrap() {
-  const app = await new App(new LoggerService());
+  const logger = new LoggerService();
+  const app = await new App(
+    logger,
+    new UserController(logger),
+    new ExeptionFilter(logger)
+  );
   app.init();
 }
 
